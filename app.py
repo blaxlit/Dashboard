924113a860535b6222d336eca4dd2bc08347f7d4
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-25 16:19:00 +0700

    Add page config and title

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv('discord_data.csv')
+
+st.set_page_config(page_title="Discord Dashboard", layout="wide")
+st.title("🎮 Discord Server Analytics")
