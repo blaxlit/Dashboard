3803eeff42048a29d917caf2f3cd5258863ceab4
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-25 16:34:53 +0700

    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ df = pd.read_csv('discord_data.csv')
 
 st.set_page_config(page_title="Discord Dashboard", layout="wide")
 st.title("🎮 Discord Server Analytics")
-st.markdown("**Welcome to the Server Analytics Dashboard**")
+
 st.sidebar.header("Filter Options")
 channel_list = df['Channel'].unique()
 selected_channel = st.sidebar.selectbox("เลือกห้อง (Channel)", channel_list)
