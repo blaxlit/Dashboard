2bdb53bed7d3216962703c869dc274dd8a262d15
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-25 16:19:21 +0700

    Add sidebar and channel selectbox

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ df = pd.read_csv('discord_data.csv')
 
 st.set_page_config(page_title="Discord Dashboard", layout="wide")
 st.title("🎮 Discord Server Analytics")
+st.sidebar.header("Filter Options")
+channel_list = df['Channel'].unique()
+selected_channel = st.sidebar.selectbox("เลือกห้อง (Channel)", channel_list)
